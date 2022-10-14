7724df9b2467dbec728c90c94e6cd1d19ef528e9
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-10-14 09:00:53 -0700

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+reate a file using Puppet
+
+file { '/tmp/holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}
